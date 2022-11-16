08f83d202db3d7249c3e68d2c2c7e77787b8c9a3
Vovan Office <plategi@yandex.ru>
2022-11-16 18:26:42 +0300

    office

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,71 @@
-import Link from "next/link";
+
 import Layout from "../components/Layout";
+import classes from '../styles/index.module.scss';
+import Link from "next/link";
+import Head from 'next/head';
+import Image from "next/image";
 
 export  default function Index() {
    return (
        <Layout title={'Civek Water Jet | Гидроабразивная резка в Москве и Москвовской области'} description={'Про Index'} keywords={'Ключ про Index'}>
-           <h1>Civek Water Jet | Гидроабразивная резка в Москве и Москвовской области</h1>
-           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita iure minima neque nostrum perspiciatis porro provident saepe temporibus veniam voluptatem?</p>
-           <p className={'text_p'}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita iure minima neque nostrum perspiciatis porro provident saepe temporibus veniam voluptatem?</p>
-           <p><Link href="/services">Service</Link></p>
-           <p><Link href={"/about"}>About</Link></p>
+          <Head>
+                <meta name='viewport' content='width=device-width,initial-scale=1'/>
+                <meta content='true' name='HandheldFriendly'/>
+                <meta content='width' name='MobileOptimized'/>
+                <meta content='yes' name='apple-mobile-web-app-capable'/>
+            </Head>
+            <div className={classes.wrapper}>
+                <div className={`${classes.item} ${classes.header}`}>
+                    <div className={classes.wrapperLogo}>
+                        <div className={`${classes.itemLogo} ${classes.logo}`}>
+                            <Link title='Сивек Water Jet' href={'/'}>
+                                <picture>
+                                    <img src="/logo.jpg" alt="Сивек Water Jet" />
+                                </picture>
+                            </Link>
+                        </div>
+                        <div className={`${classes.itemLogo} ${classes.texthead}`}>
+                            <div>
+                                <h2>Гидроабразивная резка в Москве</h2>
+                                <h3>Металл, стекло, камень, агломерат, полимеры, пластик, керамика.</h3>
+                                <p>140080 ул. Карла Маркса д. 117 &lsquo;Б&rsquo;, офис № 503 Люберецкий р-н, пос. Красково, МО. Территория института &laquo;ВНИИСТРОМ&raquo;</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div className={`${classes.item} ${classes.nav}`}>
+
+                    <ul className={classes.menunav}>
+                        <li><Link href={"/"}>Главная</Link></li>
+                        <li><Link href={"/services"}>Услуги</Link></li>
+                        <li><Link href={"/works"}>Работы</Link></li>
+                        <li><Link href={"/equipment"}>Станки</Link></li>
+                        <li><Link href={"/materials"}>Цены</Link></li>
+                        <li><Link href={"/technology"}>Технология</Link></li>
+                        <li><Link href={"/news"}>Новости</Link></li>
+                        <li><Link href={"/about"}>О компании</Link></li>
+                        <li><Link href={"/contact"}>Контакты</Link></li>
+                    </ul>
+
+                </div>
+               
+                <div className={`${classes.item} ${classes.maincontext}`}>
+                    
+                </div>
+                <div className={`${classes.item} ${classes.asideright}`}>
+                    
+                </div>
+               
+                <div className={`${classes.item} ${classes.footer}`}>
+                   <div>
+                        <p>140080 ул. Карла Маркса д. 117 &lsquo;Б&rsquo;, офис № 503 Люберецкий р-н, пос. Красково, МО. Территория института &laquo;ВНИИСТРОМ&laquo;</p>
+                   </div>
+                </div>
+                <div className={classes.footerline}>&copy; &laquo;Сивек Water Jet&raquo; 2022</div>
+            </div>
+            <div className={classes.endpage}></div>
+
        </Layout>
    );
 }
