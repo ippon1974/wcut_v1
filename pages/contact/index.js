4283b00d1253ae5fcbe1552a94bf286b0ce606e8
Vovan Office <plategi@yandex.ru>
2022-12-27 17:28:44 +0300

    o

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,13 +1,26 @@
+import React, { useState, useEffect } from 'react';
 import Layout from "../../components/layout/Layout";
 import Header from "../../components/ui/header/Header";
 import Navigation from "../../components/ui/navigation/Navigation";
+import MobileNavigation from "../../components/ui/navigation/MobileNavigation";
+import MobileFooter from "../../components/ui/footer/MobileFooter";
 import Footer from "../../components/ui/footer/Footer";
 import classes from '../../styles/contact.module.scss';
 import Link from "next/link";
 import Head from 'next/head';
 import Image from "next/image";
+import { useMediaQuery } from 'react-responsive';
 
 export default function Contact() {
+
+    const [mobile, setMobile] = useState(false)
+    const isPhone = useMediaQuery({ query: '(max-width: 481px)'})
+    useEffect(() => setMobile(isPhone), [isPhone]);
+
+    const linksmallcolor = {
+        color: "darkblue"
+    }
+
     return(
         <Layout title={'Контакты | Гидроабразивная резка в Москве и Москвовской области'} description={'Про Контакты'} keywords={'Ключ про Контакты'}>
             
@@ -20,7 +33,7 @@ export default function Contact() {
                 <Header />
                 </div>
                 <div className={`${classes.item} ${classes.nav}`}>
-                    <Navigation />
+                    {mobile ? <MobileNavigation /> : <Navigation />}
                 </div>
                 <div className={`${classes.item} ${classes.asideleft}`}>
                     <div><Image src={"/contact_intro.gif"} width={"303"} height={"229"} alt={"alt"}></Image></div>
@@ -38,25 +51,18 @@ export default function Contact() {
                         <p>Расположение производства: 140080 ул. Карла Маркса д. 117 &lsquo;Б&rsquo;, офис № 503 Люберецкий р-н, пос. Красково, МО. Территория института &laquo;ВНИИСТРОМ&raquo;</p>
                         <p>Прилегающие населенные пункты: Люберцы, Красково, Коренево, Томилино, Лыткарино, Некрасовка, Октябрьский, Котельники, Малаховка, Дзержинский, Железнодорожный, Раменское, Жуковский, Выхино-Жулебино, Электроугли.</p>
                         <p>Режим работы: Пн-Пт с 9:00 до 19:00. Суббота и воскресенье выходные дни.</p>
-                        <p>Телефон: +7 (925) 585-33-71 (Мегафон)</p>
-                        <p>Электронная почта: box@wcut.ru</p>
-                        <p className={classes.adrsmall}>Инстаграм: <Link href="https://www.instagram.com/civekwaterjet/" target="_blank">https://www.instagram.com/civekwaterjet/</Link></p>
-                        <p className={classes.adrsmall}>Ютуб: <Link href="https://www.youtube.com/channel/UCuQWXTAmeMLsruthyDSI5Gg" target="_blank">https://www.youtube.com/channel/UCuQWXTAmeMLsruthyDSI5Gg</Link></p>
-                        <p className={classes.adrsmall}>Координаты для навигатора: 55.663307,37.994535</p>
-                        <p className={classes.adrsmall}><Link href="https://www.google.com/maps/place/%D1%83%D0%BB.+%D0%9A%D0%B0%D1%80%D0%BB%D0%B0+%D0%9C%D0%B0%D1%80%D0%BA%D1%81%D0%B0,+117,+%D0%9A%D1%80%D0%B0%D1%81%D0%BA%D0%BE%D0%B2%D0%BE,+%D0%9C%D0%BE%D1%81%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB.,+%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F,+140050/@55.6616615,37.9924765,17z/data=!4m2!3m1!1s0x414ac80061d5370d:0xc82c3a4eef48e24a" target="_blank">Найти адрес компании в Google Maps</Link></p>
+                        <p><strong>Телефон</strong>: +7 (925) 585-33-71 (Мегафон)</p>
+                        <p><strong>Электронная почта</strong>: box@wcut.ru</p>
+                        <p className={classes.adrsmall}><strong>Инстаграм</strong>: <Link style={linksmallcolor} href="https://www.instagram.com/civekwaterjet/" target="_blank">https://www.instagram.com/civekwaterjet/</Link></p>
+                        <p className={classes.adrsmall}><strong>Ютуб</strong>: <Link style={linksmallcolor} href="https://www.youtube.com/channel/UCuQWXTAmeMLsruthyDSI5Gg" target="_blank">https://www.youtube.com/channel/UCuQWXTAmeMLsruthyDSI5Gg</Link></p>
+                        <p className={classes.adrsmall}><strong>Координаты для навигатора</strong>: 55.663307,37.994535</p>
+                        <p className={classes.adrsmall}><Link style={linksmallcolor} href="https://www.google.com/maps/place/%D1%83%D0%BB.+%D0%9A%D0%B0%D1%80%D0%BB%D0%B0+%D0%9C%D0%B0%D1%80%D0%BA%D1%81%D0%B0,+117,+%D0%9A%D1%80%D0%B0%D1%81%D0%BA%D0%BE%D0%B2%D0%BE,+%D0%9C%D0%BE%D1%81%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB.,+%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F,+140050/@55.6616615,37.9924765,17z/data=!4m2!3m1!1s0x414ac80061d5370d:0xc82c3a4eef48e24a" target="_blank">Найти адрес компании в Google Maps</Link></p>
                     </div>
                 </div>
 
-                <Footer />
-                {/* <div className={`${classes.footer}`}>
-                   <div>
-                        <p>140080 ул. Карла Маркса д. 117 &lsquo;Б&rsquo;, офис № 503 Люберецкий р-н, пос. Красково, МО. Территория института &laquo;ВНИИСТРОМ&laquo;</p>
-                   </div>
-                </div>
-                <div className={classes.footerline}>&copy; &laquo;Сивек Water Jet&raquo; 2022</div> */}
-
+                {mobile ? <MobileFooter /> : <Footer />}
+               
             </div>
-
 
             <div className={classes.endpage}></div>
 
