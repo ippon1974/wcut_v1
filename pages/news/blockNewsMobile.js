86e837f6744acc99c41f283c996b70ec937d507e
Vovan <plategi@yandex.ru>
2022-12-27 22:11:53 +0300

    home

--- pages/news/blockNewsMobile.js
+++ pages/news/blockNewsMobile.js
@@ -11,8 +11,8 @@ const BlockNewsMobile = () => {
                 <div className={classes.newsListMain}>
                     <p>05 March 2020</p>
                     <span>Раздел: <Link href={'#'}>статьи</Link></span>
-                    <h2><Link href="#">Первая новоить из базы данных приходит сюда. История CAM систем</Link></h2>
-                    <p className={classes.ctxNews}>История CAM систем — прошлое, настоящее и будущее</p>
+                    <h2><Link href="#">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi quam quia ad nemo doloribus.</Link></h2>
+                    <p className={classes.ctxNews}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis quaerat recusandae fugiat, odio deleniti autem pariatur excepturi commodi, numquam necessitatibus quos dicta doloremque ratione a.</p>
                 </div>
             </div>
 
@@ -20,8 +20,8 @@ const BlockNewsMobile = () => {
                 <div className={classes.newsListMain}>
                     <p>05 March 2020</p>
                     <span>Раздел: <Link href={'#'}>статьи</Link></span>
-                    <h2><Link href="#">Первая новоить из базы данных приходит сюда. История CAM систем</Link></h2>
-                    <p className={classes.ctxNews}>История CAM систем — прошлое, настоящее и будущее</p>
+                    <h2><Link href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, nesciunt at?</Link></h2>
+                    <p className={classes.ctxNews}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia temporibus corrupti voluptate, impedit quo harum?</p>
                 </div>
             </div>
 
@@ -29,8 +29,8 @@ const BlockNewsMobile = () => {
                 <div className={classes.newsListMain}>
                     <p>05 March 2020</p>
                     <span>Раздел: <Link href={'#'}>статьи</Link></span>
-                    <h2><Link href="#">Первая новоить из базы данных приходит сюда. История CAM систем</Link></h2>
-                    <p className={classes.ctxNews}>История CAM систем — прошлое, настоящее и будущее</p>
+                    <h2><Link href="#">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, fuga eligendi?</Link></h2>
+                    <p className={classes.ctxNews}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam possimus fugit perferendis labore eaque, veniam illum amet maiores?</p>
                 </div>
             </div>
 
@@ -38,8 +38,8 @@ const BlockNewsMobile = () => {
                 <div className={classes.newsListMain}>
                     <p>05 March 2020</p>
                     <span>Раздел: <Link href={'#'}>статьи</Link></span>
-                    <h2><Link href="#">Первая новоить из базы данных приходит сюда. История CAM систем</Link></h2>
-                    <p className={classes.ctxNews}>История CAM систем — прошлое, настоящее и будущее</p>
+                    <h2><Link href="#">Первая новость из базы данных приходит сюда. История CAM систем</Link></h2>
+                    <p className={classes.ctxNews}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum adipisci similique quae rerum, voluptates totam modi id praesentium, sint omnis perspiciatis, veniam magnam soluta quaerat?</p>
                 </div>
             </div>
 
