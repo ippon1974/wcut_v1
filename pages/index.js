6e1022c33a68989026f9298c7baf332d87e79a61
Vovan <plategi@yandex.ru>
2022-12-27 12:42:03 +0300

    office win

--- pages/index.js
+++ pages/index.js
@@ -67,7 +67,7 @@ export  default function Index() {
         </div>
         
         <div className={`${classes.item} ${classes.nav}`}>
-          {mobile ? <MobileNavigation /> : <Navigation />} 
+          {mobile ? <MobileNavigation /> : <Navigation />}
         </div>
        
         <div className={`${classes.item} ${classes.maincontext}`}>
@@ -82,6 +82,8 @@ export  default function Index() {
            </div>
            </Link>
            </div>
+
+           {mobile ? <Image src={'/text_logo_414_2.png'} width={'378'} height={'43'} alt={''}></Image> : ""}
 
            <div className={classes.blockDesc}>
            <h2><Link href={"/technology"}>Гидроабразивная резка</Link></h2>
@@ -99,38 +101,7 @@ export  default function Index() {
             
             {mobile ? <BlockWorkMobile /> : <BlockWork />}
 
-            {/* <div className={`${classes.worksList}`}>
-                
-                <Link href={'#'}>    
-                <div className={`${classes.viewportcontainerWork}`}>
-                    <div className={'viewportWork'} style={workPort}>
-                        <Image style={imgWork} src={'/works/1013_1.jpg'} width={'303'} height={'227'} alt={''}></Image>
-                        <Image style={imgWork} src={'/works/1013_2.jpg'} width={'303'} height={'227'} alt={''}></Image>
-                    </div>
-                </div>
-                </Link>  
-                
-                <Link href={'#'}>    
-                <div className={classes.viewportcontainerWork}>
-                    <div className={'viewportWork'} style={workPort}>
-                        <Image style={imgWork} src={'/works/8_1.jpg'} width={'303'} height={'227'} alt={''}></Image>
-                        <Image style={imgWork} src={'/works/8_2.jpg'} width={'303'} height={'227'} alt={''}></Image>
-                    </div>
-                </div>
-                </Link>
-            
-            </div> */}
-
-            {/* <div className={`${classes.worksListDesc}`}>
-                <div className={`${classes.one}`}>
-                <h2><Link href={'#'} title={'Адресная табличка. Охуенная рекомендую.'}>Адресная табличка. Охуенная рекомендую.</Link></h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus at accusantium impedit magni, nam amet doloremque quos numquam magnam autem vel quaerat cumque. Consequatur, quidem.</p>
-                </div>
-                <div className={`${classes.two}`}>
-                    <h2><Link href={'#'} title={'Очень крутая вентиляционная решетка. Покупай пока дешево!'}>Очень крутая вентиляционная решетка. Покупай пока дешево!</Link></h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus at accusantium impedit magni, nam amet doloremque quos numquam magnam autem vel quaerat cumque. Consequatur, quidem.</p>
-                </div>
-            </div> */}
+           
 
             <div className={`${classes.worksListArrowLink}`}>
                     <div className={`${classes.block_all}`}>
