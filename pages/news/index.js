735384a8247927e49a4dc22707d4e9565b65aa84
Vovan <plategi@yandex.ru>
2023-01-08 00:56:16 +0300

    H

--- pages/news/index.js
+++ pages/news/index.js
@@ -45,9 +45,6 @@ export default function News({articles:serverArticles}) {
 
             <Head>
                 <meta name="viewport" content="width=device-width"/>
-                <meta content='true' name='HandheldFriendly'/>
-                <meta content='width' name='MobileOptimized'/>
-                <meta content='yes' name='apple-mobile-web-app-capable'/>
             </Head>
 
             <div className={classes.wrapper}>
