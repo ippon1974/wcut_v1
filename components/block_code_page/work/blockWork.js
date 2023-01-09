75772fa606d49abac527dbfc5d2e1c5833465a6f
Vovan <plategi@yandex.ru>
2023-01-09 23:05:15 +0300

    home

--- components/block_code_page/work/blockWork.js
+++ components/block_code_page/work/blockWork.js
@@ -1,17 +1,9 @@
-import { useEffect, useState } from 'react'
 import classes from '../../../styles/works.module.scss';
 import Link from "next/link";
 import Image from "next/image";
 
 
 const blockWork = (props) => {
-
-    // useEffect(() => {
-    //     $(function(){
-    //         $('.viewportWork').each(function () { var imgHeight =  $(this).find('>img').css('height');  $(this).animate({ height: imgHeight, top: '-' + imgHeight }, { queue: false, duration: 100, easing: 'swing' }).css('overflow', 'visible'); });
-    //         $('.viewportWork').parent().closest('div').parent().hover(function(){$(this).find('.viewportWork').stop().animate({top: 0},{queue:false,duration:250,easing:'swing'}); }, function() { var imgHeight = $(this).find('.viewportWork').find('>img').css('height');  $(this).find('.viewportWork').stop().animate({top:'-'+imgHeight},{queue:false,duration:200,easing:'swing'}); });
-    //     })
-    // }) 
 
     const workPort = {
         height: '0',
@@ -20,15 +12,7 @@ const blockWork = (props) => {
         padding: '0',
         position:'relative'
     } 
-
-    const imgWork = {
-        top: 0,
-        left: 0,
-        border: 0,
-        display: 'block'
-    }
-
-
+    
     return (
         <>
             <div className={classes.allContainerWork}>        
@@ -37,8 +21,8 @@ const blockWork = (props) => {
                          <Link href={`/works/[id]`} as={`/works/${work.id}`} title={work.title}> 
                         <div className={`${classes.viewportcontainerWork}`}>
                             <div className={'viewportWork'} style={workPort}>
-                                <Image style={imgWork} src={`/works/small/${work.img_1}.jpg`} width={'303'} height={'227'} alt={''}></Image>
-                                <Image style={imgWork} src={`/works/small/${work.img_2}.jpg`} width={'303'} height={'227'} alt={''}></Image>
+                                <Image src={`/works/small/${work.img_1}.jpg`} width={'303'} height={'227'} alt={''}></Image>
+                                <Image src={`/works/small/${work.img_2}.jpg`} width={'303'} height={'227'} alt={''}></Image>
                             </div>
                         </div>
                         </Link> 
@@ -53,72 +37,6 @@ const blockWork = (props) => {
                      </div>
                 ))}
             </div>
-
-
-                    {/* <div className={`${classes.newListWork}`}>
-                        <Link href={'/works/id/one'}>  
-                        <div className={`${classes.viewportcontainerWork}`}>
-                            <div className={'viewportWork'} style={workPort}>
-                                <Image style={imgWork} src={'/works/1013_1.jpg'} width={'303'} height={'227'} alt={''}></Image>
-                                <Image style={imgWork} src={'/works/1013_2.jpg'} width={'303'} height={'227'} alt={''}></Image>
-                            </div>
-                        </div>
-                        </Link>
-
-                        <Link href={'/works/id/one'}>    
-                        <div className={`${classes.viewportcontainerWork}`}>
-                            <div className={'viewportWork'} style={workPort}>
-                                <Image style={imgWork} src={'/works/8_1.jpg'} width={'303'} height={'227'} alt={''}></Image>
-                                <Image style={imgWork} src={'/works/8_2.jpg'} width={'303'} height={'227'} alt={''}></Image>
-                            </div>
-                        </div>
-                        </Link>
-                        
-                        <Link href={'/works/id/one'}>    
-                        <div className={`${classes.viewportcontainerWork}`}>
-                            <div className={'viewportWork'} style={workPort}>
-                                <Image style={imgWork} src={'/works/1013_1.jpg'} width={'303'} height={'227'} alt={''}></Image>
-                                <Image style={imgWork} src={'/works/1013_2.jpg'} width={'303'} height={'227'} alt={''}></Image>
-                            </div>
-                        </div>
-                        </Link>
-                    </div> */}
-
-                    {/* <div className={`${classes.worksListDesc}`}>
-                        <div className={`${classes.one}`}>
-                        <h2><Link href={'/works/id/one'}>Адресная табличка. Охуенная рекомендую</Link></h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus at accusantium impedit magni, nam amet doloremque quos numquam magnam autem vel quaerat cumque. Consequatur, quidem.</p>
-                        </div>
-                        <div className={`${classes.two}`}>
-                            <h2><Link href={'/works/id/one'}>Очень крутая вентиляционная решетка. Покупай пока дешево!</Link></h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus at accusantium impedit magni, nam amet doloremque quos numquam magnam autem vel quaerat cumque. Consequatur, quidem.</p>
-                        </div>
-                        <div className={`${classes.two}`}>
-                            <h2><Link href={'/works/id/one'}>Очень крутая вентиляционная решетка. Покупай пока дешево!</Link></h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus at accusantium impedit magni, nam amet doloremque quos numquam magnam autem vel quaerat cumque. Consequatur, quidem.</p>
-                        </div>
-                    </div> */}
-
-                    {/* <div className={`${classes.worksListArrowLink}`}>
-                            <div className={`${classes.block_all}`}>
-                                <div className={`${classes.info_block}`}>
-                                    <div className={`${classes.info_block_arrow}`}><Link href={'#'}><Image src={'/morearrow.png'} width={'33'} height={'33'} alt=''></Image></Link></div>
-                                    <div className={`${classes.info_block_link}`}><Link href={'#'}>Подробнее</Link></div>
-                                </div>
-                            </div>
-                            <div className={`${classes.block_all}`}>
-                                <div className={`${classes.info_block}`}>
-                                    <div className={`${classes.info_block_arrow}`}><Link href={'#'}><Image src={'/morearrow.png'} width={'33'} height={'33'} alt=''></Image></Link></div>
-                                    <div className={`${classes.info_block_link}`}><Link href={'#'}>Подробнее</Link></div>
-                                </div>
-                            </div>
-                            <div className={`${classes.block_all}`}>
-                                <div className={`${classes.info_block}`}>
-                                    <div className={`${classes.info_block_arrow}`}><Link href={'#'}><Image src={'/morearrow.png'} width={'33'} height={'33'} alt=''></Image></Link></div>
-                                    <div className={`${classes.info_block_link}`}><Link href={'#'}>Подробнее</Link></div>
-                                </div>
-                            </div>
-                    </div>    */}
         </>
     )                
 }
--- pages/works/index.js
+++ pages/works/index.js
@@ -20,6 +20,13 @@ export default function Works({works:serverWorks}) {
     const isPhone = useMediaQuery({ query: '(max-width: 481px)'})
     useEffect(() => setMobile(isPhone), [isPhone]);
 
+    useEffect(() => {
+        $(function(){
+            $('.viewportWork').each(function () { var imgHeight =  $(this).find('>img').css('height');  $(this).animate({ height: imgHeight, top: '-' + imgHeight }, { queue: false, duration: 100, easing: 'swing' }).css('overflow', 'visible'); });
+            $('.viewportWork').parent().closest('div').parent().hover(function(){$(this).find('.viewportWork').stop().animate({top: 0},{queue:false,duration:250,easing:'swing'}); }, function() { var imgHeight = $(this).find('.viewportWork').find('>img').css('height');  $(this).find('.viewportWork').stop().animate({top:'-'+imgHeight},{queue:false,duration:200,easing:'swing'}); });
+        })
+    }) 
+
     const[works, setWorks] = useState(serverWorks);
 
     useEffect(()=> {
@@ -39,28 +46,6 @@ export default function Works({works:serverWorks}) {
         </Layout>
     }
 
-    useEffect(() => {
-        $(function(){
-            $('.viewportWork').each(function () { var imgHeight =  $(this).find('>img').css('height');  $(this).animate({ height: imgHeight, top: '-' + imgHeight }, { queue: false, duration: 100, easing: 'swing' }).css('overflow', 'visible'); });
-            $('.viewportWork').parent().closest('div').parent().hover(function(){$(this).find('.viewportWork').stop().animate({top: 0},{queue:false,duration:250,easing:'swing'}); }, function() { var imgHeight = $(this).find('.viewportWork').find('>img').css('height');  $(this).find('.viewportWork').stop().animate({top:'-'+imgHeight},{queue:false,duration:200,easing:'swing'}); });
-        })
-    }) 
-
-    const workPort = {
-        height: '0',
-        top:'-228px',
-        margin:'0',
-        padding: '0',
-        position:'relative'
-    }  
-
-    const imgWork = {
-        top: 0,
-        left: 0,
-        border: 0,
-        display: 'block'
-    }
-
     return(
         <Layout title={'Работы'}>
              <Head>
@@ -76,117 +61,12 @@ export default function Works({works:serverWorks}) {
                 </div>
             
                 <div className={`${classes.item} ${classes.maincontext}`}>
-                    
-                   {/* {mobile ? <BlockWorkMobile /> : <BlockWork works={works} />} */}
-
-
-                   {/* {mobile ? <BlockWorkMobile /> : 
-                   
-                   
-                   <div className={classes.allContainerWork}>        
-                   {works.map((work) => ( 
-                        <div key={work.id} className={`${classes.itemWorks}`}>
-                            <Link href={`/works/[id]`} as={`/works/${work.id}`} title={work.title}> 
-                           <div className={`${classes.viewportcontainerWork}`}>
-                               <div className={'viewportWork'} style={workPort}>
-                                   <Image style={imgWork} src={`/works/small/${work.img_1}.jpg`} width={'303'} height={'227'} alt={''}></Image>
-                                   <Image style={imgWork} src={`/works/small/${work.img_2}.jpg`} width={'303'} height={'227'} alt={''}></Image>
-                               </div>
-                           </div>
-                           </Link> 
-                           <div className={classes.allContainerWorkDesc}>
-                               <h2><Link href={`/works/[id]`} as={`/works/${work.id}`} title={work.title}>{work.title}</Link></h2>
-                               <p>{work.titlelong}</p>
-                           </div>
-                           <div className={classes.allContainerWorkArrowLink}>
-                               <div className={classes.imgLink}><Link href={`/works/[id]`} as={`/works/${work.id}`} title={'Описание работы'}><Image src={'/morearrow.png'} width={'33'} height={'33'} alt=''></Image></Link></div>
-                               <div className={classes.textLink}><Link href={`/works/[id]`} as={`/works/${work.id}`} title={'Описание работы'}>Подробнее</Link></div>
-                           </div>
-                        </div>
-                   ))}
-               </div>
-                   
-                   
-                   } */}
-                   
-                   
-                   
-              
-
-                    {/* <div className={`${classes.newListWork}`}>
-                        <Link href={'#'}>    
-                        <div className={`${classes.viewportcontainerWork}`}>
-                            <div className={'viewportWork'} style={workPort}>
-                                <Image style={imgWork} src={'/works/1013_1.jpg'} width={'303'} height={'227'} alt={''}></Image>
-                                <Image style={imgWork} src={'/works/1013_2.jpg'} width={'303'} height={'227'} alt={''}></Image>
-                            </div>
-                        </div>
-                        </Link>
-
-                        <Link href={'#'}>    
-                        <div className={`${classes.viewportcontainerWork}`}>
-                            <div className={'viewportWork'} style={workPort}>
-                                <Image style={imgWork} src={'/works/8_1.jpg'} width={'303'} height={'227'} alt={''}></Image>
-                                <Image style={imgWork} src={'/works/8_2.jpg'} width={'303'} height={'227'} alt={''}></Image>
-                            </div>
-                        </div>
-                        </Link>
-                        
-                        <Link href={'#'}>    
-                        <div className={`${classes.viewportcontainerWork}`}>
-                            <div className={'viewportWork'} style={workPort}>
-                                <Image style={imgWork} src={'/works/1013_1.jpg'} width={'303'} height={'227'} alt={''}></Image>
-                                <Image style={imgWork} src={'/works/1013_2.jpg'} width={'303'} height={'227'} alt={''}></Image>
-                            </div>
-                        </div>
-                        </Link>
-                    </div> */}
-
-
-                    
-                    {/* <div className={`${classes.worksListDesc}`}>
-                        <div className={`${classes.one}`}>
-                        <h2><Link href={'$'}>Адресная табличка. Охуенная рекомендую</Link></h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus at accusantium impedit magni, nam amet doloremque quos numquam magnam autem vel quaerat cumque. Consequatur, quidem.</p>
-                        </div>
-                        <div className={`${classes.two}`}>
-                            <h2><Link href={'$'}>Очень крутая вентиляционная решетка. Покупай пока дешево!</Link></h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus at accusantium impedit magni, nam amet doloremque quos numquam magnam autem vel quaerat cumque. Consequatur, quidem.</p>
-                        </div>
-                        <div className={`${classes.two}`}>
-                            <h2><Link href={'$'}>Очень крутая вентиляционная решетка. Покупай пока дешево!</Link></h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus at accusantium impedit magni, nam amet doloremque quos numquam magnam autem vel quaerat cumque. Consequatur, quidem.</p>
-                        </div>
-                    </div> */}
-
-                    {/* <div className={`${classes.worksListArrowLink}`}>
-                            <div className={`${classes.block_all}`}>
-                                <div className={`${classes.info_block}`}>
-                                    <div className={`${classes.info_block_arrow}`}><Link href={'#'}><Image src={'/morearrow.png'} width={'33'} height={'33'} alt=''></Image></Link></div>
-                                    <div className={`${classes.info_block_link}`}><Link href={'#'}>Подробнее</Link></div>
-                                </div>
-                            </div>
-                            <div className={`${classes.block_all}`}>
-                                <div className={`${classes.info_block}`}>
-                                    <div className={`${classes.info_block_arrow}`}><Link href={'#'}><Image src={'/morearrow.png'} width={'33'} height={'33'} alt=''></Image></Link></div>
-                                    <div className={`${classes.info_block_link}`}><Link href={'#'}>Подробнее</Link></div>
-                                </div>
-                            </div>
-                            <div className={`${classes.block_all}`}>
-                                <div className={`${classes.info_block}`}>
-                                    <div className={`${classes.info_block_arrow}`}><Link href={'#'}><Image src={'/morearrow.png'} width={'33'} height={'33'} alt=''></Image></Link></div>
-                                    <div className={`${classes.info_block_link}`}><Link href={'#'}>Подробнее</Link></div>
-                                </div>
-                            </div>
-                    </div>     */}
-
-
+                   {mobile ? <BlockWorkMobile /> : <BlockWork works={works} />}
                 </div>
 
                <div className={classes.componentfooter}>
                 {mobile ? <MobileFooter /> : <Footer />}
                </div>         
-               
             </div>
 
             <div className={classes.endpage}></div>
