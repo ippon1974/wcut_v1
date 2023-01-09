8d47b6d875edd0f7818914dd7b6675b348af0c87
Vovan Office <plategi@yandex.ru>
2023-01-09 19:04:29 +0300

    o

--- pages/works/index.js
+++ pages/works/index.js
@@ -43,23 +43,23 @@ export default function Works({works:serverWorks}) {
         $(function(){
             $('.viewportWork').each(function () { var imgHeight =  $(this).find('>img').css('height');  $(this).animate({ height: imgHeight, top: '-' + imgHeight }, { queue: false, duration: 100, easing: 'swing' }).css('overflow', 'visible'); });
             $('.viewportWork').parent().closest('div').parent().hover(function(){$(this).find('.viewportWork').stop().animate({top: 0},{queue:false,duration:250,easing:'swing'}); }, function() { var imgHeight = $(this).find('.viewportWork').find('>img').css('height');  $(this).find('.viewportWork').stop().animate({top:'-'+imgHeight},{queue:false,duration:200,easing:'swing'}); });
-        }, [])
+        })
     }) 
 
-    // const workPort = {
-    //     height: '0',
-    //     top:'-228px',
-    //     margin:'0',
-    //     padding: '0',
-    //     position:'relative'
-    // }  
+    const workPort = {
+        height: '0',
+        top:'-228px',
+        margin:'0',
+        padding: '0',
+        position:'relative'
+    }  
 
-    // const imgWork = {
-    //     top: 0,
-    //     left: 0,
-    //     border: 0,
-    //     display: 'block'
-    // }
+    const imgWork = {
+        top: 0,
+        left: 0,
+        border: 0,
+        display: 'block'
+    }
 
     return(
         <Layout title={'Работы'}>
@@ -77,7 +77,41 @@ export default function Works({works:serverWorks}) {
             
                 <div className={`${classes.item} ${classes.maincontext}`}>
                     
-                   {mobile ? <BlockWorkMobile /> : <BlockWork works={works} />}
+                   {/* {mobile ? <BlockWorkMobile /> : <BlockWork works={works} />} */}
+
+
+                   {/* {mobile ? <BlockWorkMobile /> : 
+                   
+                   
+                   <div className={classes.allContainerWork}>        
+                   {works.map((work) => ( 
+                        <div key={work.id} className={`${classes.itemWorks}`}>
+                            <Link href={`/works/[id]`} as={`/works/${work.id}`} title={work.title}> 
+                           <div className={`${classes.viewportcontainerWork}`}>
+                               <div className={'viewportWork'} style={workPort}>
+                                   <Image style={imgWork} src={`/works/small/${work.img_1}.jpg`} width={'303'} height={'227'} alt={''}></Image>
+                                   <Image style={imgWork} src={`/works/small/${work.img_2}.jpg`} width={'303'} height={'227'} alt={''}></Image>
+                               </div>
+                           </div>
+                           </Link> 
+                           <div className={classes.allContainerWorkDesc}>
+                               <h2><Link href={`/works/[id]`} as={`/works/${work.id}`} title={work.title}>{work.title}</Link></h2>
+                               <p>{work.titlelong}</p>
+                           </div>
+                           <div className={classes.allContainerWorkArrowLink}>
+                               <div className={classes.imgLink}><Link href={`/works/[id]`} as={`/works/${work.id}`} title={'Описание работы'}><Image src={'/morearrow.png'} width={'33'} height={'33'} alt=''></Image></Link></div>
+                               <div className={classes.textLink}><Link href={`/works/[id]`} as={`/works/${work.id}`} title={'Описание работы'}>Подробнее</Link></div>
+                           </div>
+                        </div>
+                   ))}
+               </div>
+                   
+                   
+                   } */}
+                   
+                   
+                   
+              
 
                     {/* <div className={`${classes.newListWork}`}>
                         <Link href={'#'}>    
