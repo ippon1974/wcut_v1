714650d42ccd5997ab2756933e6dabe34d73e8ee
Vovan Office <plategi@yandex.ru>
2023-01-10 19:15:12 +0300

    o

--- pages/works/[id].js
+++ pages/works/[id].js
@@ -10,20 +10,45 @@ import MobileFooter from "../../components/ui/footer/mobile/MobileFooter";
 import Footer from "../../components/ui/footer/main/Footer";
 import classes from '../../styles/works_id.module.scss';
 import Head from 'next/head';
-
-
-
 import { useMediaQuery } from 'react-responsive';
 
 
 const Work = ({work:serverWork}) => {
 
+    const router = useRouter();
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        // fetch data
+        const dataFetch = async () => {
+          const data = await (
+            await fetch(
+            //   "https://run.mocky.io/v3/b3bcb9d2-d8e9-43c5-bfb7-0062c85be6f9"
+              `http://localhost:7000/works/next/${router.query.id}`
+            )
+          ).json();
+    
+          // set state when the data received
+          setData(data);
+        };
+        dataFetch();
+      }, []);
+      
+      var mydata = [data];
+      const myJSON = JSON.stringify(data);
+      console.log(mydata[0]);
+      mydata.forEach(x => console.log(x));
+
+
+      
+      
     const[mobile, setMobile] = useState(false)
     const isPhone = useMediaQuery({ query: '(max-width: 481px)'})
     useEffect(() => setMobile(isPhone), [isPhone]);
 
+    
+
     const[work, setWork] = useState(serverWork);
-    const router = useRouter();
 
     useEffect(()=>{
         async function load() {
@@ -36,12 +61,14 @@ const Work = ({work:serverWork}) => {
         }
     },[])// eslint-disable-line react-hooks/exhaustive-deps
 
+
     if(!work){
         return <Layout>
             <p>...Loading</p>
         </Layout>
     }
 
+   
     return (
         <Layout title={'Работы'}>
 
@@ -72,11 +99,21 @@ const Work = ({work:serverWork}) => {
 }
 export default Work;
 
+// export async function getServerSideProps({query, req}) {
+//     if(!req){
+//         return {prev:null}
+//     }
+//     const response = await fetch(`http://localhost:7000/works/prev/${query.id}`);
+//     const prev = await response.json();
+//     return {props: {prev}}
+// }
+
+
 export async function getServerSideProps({query, req}) {
     if(!req){
         return {work:null}
     }
     const response = await fetch(`http://localhost:7000/works?id=${query.id}`);
     const work = await response.json();
-    return {props: {work}}
+    return {props: {work} }
 }
