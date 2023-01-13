65a115ad81366eb3a3737e58f53779c177d61c92
Vovan <plategi@yandex.ru>
2023-01-14 00:39:21 +0300

    home

--- pages/materials/index.js
+++ pages/materials/index.js
@@ -10,12 +10,31 @@ import Link from "next/link";
 import Head from 'next/head';
 import Image from "next/image";
 import { useMediaQuery } from 'react-responsive'; 
+import Select from "react-select";
 
 export default function Price() {
 
     const [mobile, setMobile] = useState(false)
     const isPhone = useMediaQuery({ query: '(max-width: 481px)'})
     useEffect(() => setMobile(isPhone), [isPhone]);
+
+    const [aglomerat, setAglomerat] = useState('215');
+    const[brass, setBrass] = useState('278');
+    const[steel, setSteel] = useState('418');
+
+
+    // const options = [
+    //     { value: "blues", label: "Blues" },
+    //     { value: "rock", label: "Rock" },
+    //     { value: "jazz", label: "Jazz" },
+    //     { value: "orchestra", label: "Orchestra" },
+    //   ];
+
+    // function handleChange(event) {
+	// 	setValue(event.target.value);
+	// }
+
+
 
     return(
         <Layout title={'Цены на раскрой гидроабразивом | Стоимость раскроя различных материалов с помощью технологии гидроабразивной резки.'}>
@@ -47,6 +66,32 @@ export default function Price() {
                         <p>Стоимость раскроя материала указана в рублях РФ за один погонный метр <strong>без НДС</strong>. Минимальная стоимость заказа не должна быть <strong>меньше 5000 рублей</strong>.</p>
                         <p>Стоимость программы для раскроя — 2500 рублей.</p>
                         
+                        {/* <select value={value} onChange={handleChange}>
+                            <option value={'o'}>text1</option>
+                            <option>text2</option>
+                            <option>text3</option>
+                            <option>text4</option>
+                        </select>
+                        <p>ваш выбор: {value}</p> */}
+
+                        {/* <select value={value} onChange={(event) => setValue(event.target.value)}>
+                                    <option value="5">5</option>
+                                    <option value="10">10</option>
+                                    <option value="15">15</option>
+                                    <option value="20">20</option>
+                                    <option value="25">25</option>
+                                    <option value="30">30</option>
+                                    <option value="35">35</option>
+                                    <option value="40">40</option>
+                                    <option value="45">45</option>
+                                    <option value="50">50</option>
+                        </select>
+                        <p>
+                            ваш выбор: {value}
+                        </p> */}
+
+                        {/* <Select options={options} /> */}
+
                         <h3>Таблица стоимости раскроя материала гидроабразивом</h3>
                         
                         <table className={classes.pricematerials}>
@@ -58,19 +103,60 @@ export default function Price() {
                             </tr>
                             <tr>
                                 <td><a href={'/materials/id/one'} title="Агломерат. 10 мм. 960 руб. 1 пог. метр. Гидроабразив">Агломерат</a></td>
-                                <td>10 мм.</td>
-                                <td>960 руб. <span class="short">пог. м.</span></td>
+                                <td>
+                                <select value={aglomerat} onChange={(event) => setAglomerat(event.target.value)}>
+                                    <option value="215">5</option>
+                                    <option value="355">10</option>
+                                    <option value="485">15</option>
+                                    <option value="525">20</option>
+                                    <option value="680">25</option>
+                                    <option value="788">30</option>
+                                    <option value="855">35</option>
+                                    <option value="950">40</option>
+                                    <option value="1050">45</option>
+                                    <option value="1350">50</option>
+                                </select> мм.
+                                </td>
+                                <td>{aglomerat} руб. <span class="short">пог. м.</span></td>
                             </tr>
                             <tr>
-                                <td><a href={"/materials/id/one"} title="Агломерат. 10 мм. 960 руб. 1 пог. метр. Гидроабразив">Агломерат</a></td>
-                                <td>10 мм.</td>
-                                <td>960 руб. <span class="short">пог. м.</span></td>
+                                <td><a href={"/materials/id/one"} title="Агломерат. 10 мм. 960 руб. 1 пог. метр. Гидроабразив">Латунь</a></td>
+                                <td>
+                                <select value={brass} onChange={(event) => setBrass(event.target.value)}>
+                                    <option value="278">5</option>
+                                    <option value="378">10</option>
+                                    <option value="478">15</option>
+                                    <option value="579">20</option>
+                                    <option value="615">25</option>
+                                    <option value="735">30</option>
+                                    <option value="815">35</option>
+                                    <option value="988">40</option>
+                                    <option value="1150">45</option>
+                                    <option value="1385">50</option>
+                                </select> мм.
+                                </td>
+                                <td>{brass} руб. <span class="short">пог. м.</span></td>
                             </tr>
+
                             <tr>
-                                <td><a href={"/materials/id/one"} title="Агломерат. 10 мм. 960 руб. 1 пог. метр. Гидроабразив">Агломерат</a></td>
-                                <td>10 мм.</td>
-                                <td>960 руб. <span class="short">пог. м.</span></td>
+                                <td><a href={"/materials/id/one"} title="Агломерат. 10 мм. 960 руб. 1 пог. метр. Гидроабразив">Нержавеющая сталь</a></td>
+                                <td>
+                                <select value={steel} onChange={(event) => setSteel(event.target.value)}>
+                                    <option value="418">5</option>
+                                    <option value="535">10</option>
+                                    <option value="682">15</option>
+                                    <option value="766">20</option>
+                                    <option value="825">25</option>
+                                    <option value="818">30</option>
+                                    <option value="1018">35</option>
+                                    <option value="1288">40</option>
+                                    <option value="1378">45</option>
+                                    <option value="1546">50</option>
+                                </select> мм.
+                                </td>
+                                <td>{steel} руб. <span class="short">пог. м.</span></td>
                             </tr>
+                           
                             </tbody>
                         </table>
 
