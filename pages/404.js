a1fb084cceb978a66c48012f5a4fd82a6edece4f
Vovan Office <plategi@yandex.ru>
2023-02-20 14:09:12 +0300

    o

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,28 @@
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-import {MainLayout} from '../components/layout/MainLayout';
 import classes from '../styles/error.module.scss';
+import { useMediaQuery } from 'react-responsive';
 
 export default function ErrorPage() {
+
+    const [mobile, setMobile] = useState(false)
+    const isPhone = useMediaQuery({ query: '(max-width: 481px)'})
+    useEffect(() => setMobile(isPhone), [isPhone]);
+
     return(
-        <MainLayout title={'Error 404'}>
-            <h1 className={classes.error}>Error 404</h1>
-            <p><Link href={'/'}>Page Idex</Link></p>
-        </MainLayout>
+        <>
+         {mobile ?  
+         <div className={classes.container404Mobile}>
+                <div>
+                    <h1><Link href={'/'} title={'На главную'}>404</Link></h1>
+                </div>
+        </div> :  
+        <div className={classes.container404}>
+                <div>
+                    <h1><Link href={'/'} title={'На главную'}>404</Link></h1>
+                </div>
+        </div>} 
+           
+        </>
     );
 }
--- pages/500.js
+++ pages/500.js
@@ -1,3 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import classes from '../styles/error.module.scss';
+import { useMediaQuery } from 'react-responsive';
+
 export default function Custom500() {
-    return <h1>500 - Server-side error occurred</h1>
+  const [mobile, setMobile] = useState(false)
+  const isPhone = useMediaQuery({ query: '(max-width: 481px)'})
+  useEffect(() => setMobile(isPhone), [isPhone]);
+    
+  return (
+    <>
+       {mobile ?  
+         <div className={classes.container500Mobile}>
+                <div>
+                    <h1><Link href={'/'} title={'На главную'}>500</Link></h1>
+                </div>
+        </div> :  
+        <div className={classes.container500}>
+                <div>
+                    <h1><Link href={'/'} title={'На главную'}>500</Link></h1>
+                </div>
+        </div>} 
+    </>
+    )
   }
