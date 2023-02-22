16378f913be2aa7f7b75c913fb95d0fd55ca3ed0
Vovan <plategi@yandex.ru>
2023-02-23 00:41:21 +0300

    h

--- pages/api/works/next/[id].js
+++ pages/api/works/next/[id].js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     const { id } = req.query
-    const resworks = await fetch(`http://23.105.246.179:7000/works/next?id=${id}`);
+    const resworks = await fetch(`http://23.105.246.179:7000/works/next?id=${req.query.id}`);
     const data = await resworks.json();
     res.json(data);
   }
--- pages/works/[id].js
+++ pages/works/[id].js
@@ -55,7 +55,7 @@ const Work = ({work:serverWork, prev:serverPrev, next:serverNext, maxid:serverMa
     useEffect(()=>{
        async function load() {
         // const response = await fetch(`http://23.105.246.179:7000/works/next?id=${router.query.id}`);
-        const response = await fetch(`http://23.105.246.179:3000/api/works/next/${router.query.id}`);
+        const response = await fetch(`/api/works/next/${router.query.id}`);
         const data = await response.json();
         if(data[0] == undefined){
             return
