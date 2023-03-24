c9076e308a8ec3b5f4145231b3d48e173f0f3b2a
Vovan Office <plategi@yandex.ru>
2023-03-24 08:46:33 +0300

    o

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
 module.exports = nextConfig
+
+
+module.exports = {
+  // All the other config options you may have...
+  async redirects() {
+      return [
+          {
+              source: '/news/id/1040',
+              destination: '/news/4',
+              permanent: false
+          }
+      ];
+  }
+};
