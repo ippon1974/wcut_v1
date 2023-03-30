f3009685a7d2709c4bf36f2f86af36cbe5319e9a
Vovan Office <plategi@yandex.ru>
2023-03-30 17:04:47 +0300

    o

--- pages/_document.js
+++ pages/_document.js
@@ -16,14 +16,12 @@ export default class MyDocument extends Document {
                         window.dataLayer = window.dataLayer || [];
                         function gtag(){dataLayer.push(arguments);}
                         gtag('js', new Date());
-                      
                         gtag('config', 'G-C9ZNZ01609');
-                      `
                       }}
                     />
 
                 </Head>
-                
+
                 <body>
                 <Main /> 
                 <NextScript />
