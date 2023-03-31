9a3bbbded330f0258a3c50537986dcfe987ca1ab
Vovan Office <plategi@yandex.ru>
2023-03-31 10:31:04 +0300

    O

--- pages/_document.js
+++ pages/_document.js
@@ -9,19 +9,6 @@ export default class MyDocument extends Document {
                     {/* <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" /> */}
                     <link rel="shortcut icon" href="/icon.ico" type="image/x-icon"></link>
                     <link type="image/png" sizes="120x120" rel="icon" href="/favicon-120x120.png"></link>
-
-                    {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-C9ZNZ01609"></script>
-                    <script
-                    dangerouslySetInnerHTML={{__html: `
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-
-                        gtag('config', 'G-C9ZNZ01609');
-                      `
-                      }}
-                    /> */}
-
                 </Head>
 
                 <body>
